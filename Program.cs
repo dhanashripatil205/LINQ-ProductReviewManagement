6e68b736376c04e01fc0fec506a9d9d0d745265a
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-02-05 00:28:14 +0530

    Comment Out!!!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -41,4 +42,4 @@ namespace ProductReviewManagement
             
         }
     }
-}
+}*/
